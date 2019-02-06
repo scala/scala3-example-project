6b4d2a834d963121fd9fd85ec32d6c313b86140a
Alvin Alexander <al@alvinalexander.com>
2019-02-06 12:34:40 -0700

    updated url

--- src/main/scala/ImplicitConversion.scala
+++ src/main/scala/ImplicitConversion.scala
@@ -1,7 +1,7 @@
 import scala.language.implicitConversions
 
 /**
-  * Implicit Conversions: http://dotty.epfl.ch/docs/reference/changed/implicit-conversions.html
+  * Implicit Conversions: http://dotty.epfl.ch/docs/reference/changed-features/implicit-conversions.html
   */
 object ImplicitConversion {
 

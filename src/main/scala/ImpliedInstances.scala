76bf0b4d708206b1901fa7f291f07cd470506e79
Anatolii <anatoliykmetyuk@gmail.com>
2019-05-23 16:28:46 +0200

    Upgrade Dotty to 0.15.0-RC1

--- src/main/scala/ImpliedInstances.scala
+++ src/main/scala/ImpliedInstances.scala
@@ -21,7 +21,7 @@ object ImpliedInstances {
     implied stringParser for StringParser[String] = baseParser(Success(_))
     implied intParser for StringParser[Int] = baseParser(s ⇒ Try(s.toInt))
 
-    implied optionParser[A] given (parser: => StringParser[A]) for StringParser[Option[A]] = new StringParser[Option[A]] {
+    implied optionParser[A] for StringParser[Option[A]] given (parser: => StringParser[A]) = new StringParser[Option[A]] {
       override def parse(s: String): Try[Option[A]] = s match {
         case "" ⇒ Success(None) // implicit parser not used.
         case str ⇒ parser.parse(str).map(x ⇒ Some(x)) // implicit parser is evaluated at here

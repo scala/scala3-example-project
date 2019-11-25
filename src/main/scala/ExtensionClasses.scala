a75eb03a288320014ec8ff59c66ac1c050987d79
Mike Slinn <mslinn@micronauticsresearch.com>
2019-11-25 16:25:35 -0500

    -

--- src/main/scala/ExtensionClasses.scala
+++ src/main/scala/ExtensionClasses.scala
@@ -1,14 +1,30 @@
-object ExtensionClasses extends App {
+/** See https://dotty.epfl.ch/docs/reference/contextual/extension-methods-new.html */
+object ExtensionClasses1 extends App {
   case class Circle(x: Double, y: Double, radius: Double)
 
   // New Scala 3 way:
   def (c: Circle) circumference: Double = c.radius * math.Pi * 2
 
-  val value = Circle(0, 0, 0.5).circumference
-  println(s"circumference = $value")
+  val circle = Circle(0, 0, 0.5)
+  println(s"circumference = ${ circle.circumference }")
+
+  assert(circle.circumference == circumference(circle))
 
   // Scala 2 way
   //implicit class CircleOps(circle: Circle) extends AnyVal {
   //  def circumference = circle.radius * Pi * 2
   //}
+}
+
+object ExtensionClasses2 extends App {
+  trait StringSeqOps {
+    def (xs: Seq[String]) longestStrings = {
+      val maxLength = xs.map(_.length).max
+      xs.filter(_.length == maxLength)
+    }
+  }
+
+  given ops1: StringSeqOps
+
+  println(s"""List("here", "is", "a", "list").longestStrings: """ + List("here", "is", "a", "list").longestStrings)
 }

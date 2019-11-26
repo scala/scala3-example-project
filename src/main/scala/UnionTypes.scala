c65727b6d47980763a60671ba1974c571105c2d5
Mike Slinn <mslinn@micronauticsresearch.com>
2019-11-26 15:09:34 -0500

    -

--- src/main/scala/UnionTypes.scala
+++ src/main/scala/UnionTypes.scala
@@ -1,15 +1,17 @@
-/** Union Types: https://dotty.epfl.ch/docs/reference/new-types/union-types.html */
+/** Union types (aka sum types): https://dotty.epfl.ch/docs/reference/new-types/union-types.html */
 object UnionTypes extends App {
   sealed trait Division
   final case class DivisionByZero(msg: String) extends Division
   final case class Success(double: Double) extends Division
 
-  // You can create type aliases for your union types (sum types).
+  // Type aliases can use union types
   type DivisionResult = DivisionByZero | Success
 
   sealed trait List[+A]
-  final class Empty extends List[Nothing]
   final case class Cons[+A](h: A, t: List[A]) extends List[A]
+  final class Empty extends List[Nothing] {
+    override def toString: String = "Empty"
+  }
 
   private def safeDivide(a: Double, b: Double): DivisionResult =
     if (b == 0) DivisionByZero("DivisionByZeroException") else Success(a / b)
@@ -26,15 +28,16 @@ object UnionTypes extends App {
     val divisionResultFailure: Success | DivisionByZero = safeDivide(4, 0)
 
     // Calling `either` function with union typed value.
-    println(either(divisionResultSuccess))
+    println("either(divisionResultSuccess): " + either(divisionResultSuccess))
 
     // Calling `either` function with union typed value.
-    println(either(divisionResultFailure))
+    println("either(divisionResultFailure): " + either(divisionResultFailure))
 
     val list: Cons[Int] | Empty = Cons(1, Cons(2, Cons(3, Empty())))
+    println("list: " + list)
+
     val emptyList: Empty | Cons[Any] = Empty()
-    println(list)
-    println(emptyList)
+    println("emptyList: " + emptyList)
   }
 
   test

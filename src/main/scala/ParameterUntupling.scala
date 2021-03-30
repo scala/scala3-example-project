e39a226fd3ede9ebfc076fdfbd3caef34673589d
Guillaume Martres <smarter@ubuntu.com>
2021-03-30 23:43:21 +0200

    Remove outdated information on lazy val
    Fixes #79.

--- src/main/scala/ParameterUntupling.scala
+++ src/main/scala/ParameterUntupling.scala
@@ -4,11 +4,7 @@
 object ParameterUntupling:
 
   def test(): Unit =
-    /**
-      * In order to get thread safety, you need to put @volatile before lazy vals.
-      * https://dotty.epfl.ch/docs/reference/changed-features/lazy-vals-init.html
-      */
-    @volatile lazy val xs: List[String] = List("d", "o", "t", "t", "y")
+    val xs: List[String] = List("d", "o", "t", "t", "y")
 
     /**
       * Current behaviour in Scala 2.12.2 :
